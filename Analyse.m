6d7815f4bc40720125b5d6dacf3873a228e59573
Aureco998 <aurelie.coppex@gmail.com>
2020-10-22 18:22:56 +0200

    Suite exo ai

--- Analyse.m
+++ Analyse.m
@@ -104,6 +104,72 @@ grid on
 
 %-------------------------Partie a-------------------------------------
 
+%----------------------------i)-----------------------------------------
+
+%Convergence de l'erreur de la position -------- Euler explicite
+
+nsimul  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+error_pos = [0.0014, 
+             9.7108e-04,
+             5.8968e-04,
+             3.8472e-04,
+             2.5421e-04,
+             1.6935e-04,
+             1.1468e-04,
+             7.8801e-05,
+             5.3915e-05,
+             3.7003e-05,
+             2.5599e-05,
+             1.7684e-05,
+             1.2259e-05,
+             8.5042e-06,
+             5.9026e-06,
+             4.0989e-06,
+             2.8475e-06,
+             1.9786e-06,
+             1.3750e-06,
+             9.5563e-07
+    ];
+
+lw=2; fs=16;
+figure('Name', [filename ': Erreur sur la position selon Euler explicite'])
+plot(1./nsimul, error_pos, 'k+-','linewidth',lw, "Color", "blue")
+set(gca,'fontsize',fs)
+xlabel('1/N simulation')
+ylabel('error position max')
+grid on
+
+%Convergence de l'erreur de la vitesse----------Euler explicite
+nsimul  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+error_pos = [6.9847e+05,
+            4.7275e+05,
+            2.9008e+05,
+            1.9191e+05,
+            1.2940e+05,
+            8.8760e+04,
+            6.2577e+04,
+            5.0867e+04,
+            5.1260e+04,
+            5.1639e+04,
+            5.1947e+04,
+            5.2185e+04,
+            5.2360e+04,
+            5.2486e+04,
+            5.2576e+04,
+            5.2640e+04,
+            5.2685e+04,
+            5.2717e+04,
+            5.2739e+04,
+            5.2754e+04];
+
+lw=2; fs=16;
+figure('Name', [filename ': Erreur sur la vitesse selon Euler explicite'])
+plot(1./nsimul, error_pos, 'k+-','linewidth',lw, "Color", "blue")
+set(gca,'fontsize',fs)
+xlabel('1/N simulation')
+ylabel('error vitesse max')
+grid on
+
 %-----------------------------ii)---------------------------------------
 % Non-conservation de l'énergie mécanique 
 E_mec_i = (1.0/2.0 * 1.62726e-27*4e5*4e5);
@@ -114,7 +180,7 @@ E_mec_t = [7.75697638819292e-007,
             7.77614331728026e-007,
             7.77780741836893e-007 ]
 delta_E_mec = E_mec_t-E_mec_0;
-plot(nsteps_num, abs(delta_E_mec,'k+-', "Color", "blue")
+plot(nsteps_num, abs(delta_E_mec),'k+-', "Color", "blue")
 set(gca,'fontsize',fs)
 set(gca,'xscale','log')
 set(gca,'yscale','log')
--- ParameterScan.m
+++ ParameterScan.m
@@ -57,9 +57,12 @@ for i = 1:nsimul % Parcours des resultats de toutes les simulations
     x_th = ((1.6726e-27/(1.6022e-19*5))*4e5*cos(1.6022e-19*5*1.e-8/1.6726e-27)-(1.6726e-27*4e5/(1.6022e-19*5))); % TODO: Entrer la vraie solution analytique a tfin
     z_th = ((4e5*1.6726e-27/(1.6022e-19*5))*sin(1.6022e-19*5*1.e-8/1.6726e-27)); % TODO: Entrer la vraie solution analytique a tfin
     error(i) = sqrt((xend-x_th)^2+(zend-z_th)^2); % erreur sur la position finale
-    error_x(i) = sqrt((xend-x_th)^2);
-    error_z(i) = sqrt((zend-z_th)^2);
+    error_x(i) = abs(xend-x_th); % Calcul de l'erreur locale en x
+    error_z(i) = abs(zend-z_th); % Calcul de l'erreur locale en z
+    
+    erreur_pos_max = max(error_x(i), error_z(i)) % Maximum de l'erreur sur la position
 end
+
 
 
 
@@ -68,7 +71,7 @@ loglog(dt, error_x, 'k+',"Color", "blue")
 hold on
 loglog(dt, error_z, 'k+', "Color", "magenta")
 xlabel('\Delta t')
-ylabel('Maximum de l''erreur sur la position')
+ylabel('Erreur locale sur la position (x(b), z(m))')
 grid on
  
  x = x_th %Pour écrire dans la fenêtre (sans ;)
@@ -77,6 +80,8 @@ grid on
 
 %Erreur max sur la vitesse
 error = zeros(1,nsimul);
+error_v_x = zeros(1, nsimul);
+error_v_z = zeros(1, nsimul);
 for i = 1:nsimul % Parcours des resultats de toutes les simulations
     data = load(output{i}); % Chargement du fichier de sortie de la i-ieme simulation
     dt(i) = data(2,1)-data(1,1); % Delta t = t_2-t_1
@@ -85,12 +90,19 @@ for i = 1:nsimul % Parcours des resultats de toutes les simulations
     v_x_th = (-4e5*sin(1.6022e-19*5*1.e-8/1.62726e-27)); % TODO: Entrer la vraie solution analytique a tfin
     v_z_th = (4e5*cos(1.6022e-19*5*1.e-8/1.62726e-27)); % TODO: Entrer la vraie solution analytique a tfin
     error(i) = sqrt((v_xend-v_x_th)^2+(v_zend-v_z_th)^2); % erreur sur la position finale
+    error_v_x(i) = abs(v_xend-v_x_th); %Calcul de l'erreur locale en x
+    error_v_z(i) = abs(v_zend-v_z_th); %Calcul de l'erreur locale en z
+
+    erreur_vit_max = max(error_v_x(i), error_v_z(i)) % Maximum de l'erreur sur la position
 end
 
+
 figure
-loglog(dt, error, 'k+')
+loglog(dt, error_v_x, 'k+',"Color", "blue")
+hold on
+loglog(dt, error_v_z, 'k+', "Color", "magenta")
 xlabel('\Delta t')
-ylabel('Maximum de l''erreur sur la vitesse')
+ylabel('Erreur locale de la vitesse (v_x(b), v_z(m))')
 grid on
 
 v_x = v_x_th
